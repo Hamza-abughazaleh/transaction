b8dd772493a17d00f04c0e0ae10fe6c3a5f526d7
hamza <hghazaleh@aswaq.com>
2018-12-18 21:31:35 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="transaction",
+    version="0.0.1",
+    author="Hamza abughazaleh",
+    author_email="hamzaabughazaleh23@gmail.com",
+    description="A small example package",
+    url="",
+    packages=setuptools.find_packages(),
+)
