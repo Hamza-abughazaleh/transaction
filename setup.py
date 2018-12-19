259ca72454750bed4d23b851e1a0d9699dd77f81
hamza <hghazaleh@aswaq.com>
2018-12-19 12:16:41 +0200

    second

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
 setuptools.setup(
-    name="transaction",
-    version="0.0.1",
-    author="Hamza abughazaleh",
-    author_email="hamzaabughazaleh23@gmail.com",
-    description="A small example package",
-    url="",
-    packages=setuptools.find_packages(),
+   name="transaction_pkg",
+   version="0.0.1",
+   author="Hamza",
+   author_email="author@example.com",
+   description="A small example package",
+   url="https://github.com/Hamza-abughazaleh/transaction",
+   packages=setuptools.find_packages(),
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
 )
